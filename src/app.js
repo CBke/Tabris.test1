8dfeaed1502f69da0815fc55199ba7ce23bac77d
CBke <cbatdotcom@gmail.com>
2017-12-01 22:49:11 +0100

    initial test

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
-const {Button, TextView, ui} = require('tabris');
+const {Button, CollectionView, Composite, ImageView, TextView, ScrollView, ui} = require('tabris');
+const IMAGE_PATH = 'images/';
 
-let button = new Button({
-  centerX: 0, top: 100,
-  text: 'Show message'
-}).appendTo(ui.contentView);
+let scrollView = new ScrollView({left: 0, top: 0, right: 0, bottom: 0}).appendTo(ui.contentView);
 
-let textView = new TextView({
-  centerX: 0, top: [button, 50],
-  font: '24px'
-}).appendTo(ui.contentView);
-
-button.on('select', () => {
-  textView.text = 'Tabris.js rocks!';
+fetch('http://cursussen.uantwerpen.be/Home/Level')
+    .then(response => response.json())
+    .then((json) => {
+    
+    for (var i = 0; i < json.Items.length; i++){
+    let text=  json.Items[i].Item;
+        new Button({
+          left: 16, right: 16, top: 'prev()',
+          text: text +  i ,
+          id: 'reloadButton'
+        })
+        .appendTo(scrollView);
+    }
 });
