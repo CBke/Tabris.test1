5ba7f2f3ad16ab4ccd418b5bba9e84428d7833db
CBke <cbatdotcom@gmail.com>
2017-12-02 03:10:44 +0100

    cleanup and some details

--- src/app.js
+++ src/app.js
@@ -1,50 +1,70 @@
-// Create a collection view, initialize its cells and fill it with items
-const { Button, CollectionView, Composite, ImageView, TextView, ScrollView, NavigationView,Page, ui} = require('tabris');
+const {
+    Button,
+    CollectionView,
+    Composite,
+    ImageView,
+    TextView,
+    ScrollView,
+    NavigationView,
+    Page,
+    ui
+} = require('tabris');
 let json = "";
 
 let navigationView = new NavigationView({
-  left: 0, top: 0, right: 0, bottom: 0,
-  drawerActionVisible: true
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    drawerActionVisible: true
 }).appendTo(ui.contentView);
-  
-fetch('http://cursussen.uantwerpen.be/Home/Level')
-  .then(response => response.json())
-  .then((jso) => json = jso)
-  .then(() => CreatePage('')); 
 
-function CreatePage(Route)
-{
-  
-  let path = Route.split("/").map(Number);
-  
-  switch (path.length) {
-  case 1:
-       console['info']('path.length :' + path.length);
-    FillWithData(json, Route)
-  break;
-  case 2:
-    FillWithData(json.Items[path[0]], Route)
-  break;
-  case 3:
-    FillWithData(json.Items[path[0]].Items[path[1]], Route)
-  break;
-  case 4:
-    FillWithData(json.Items[path[0]].Items[path[1]].Items[path[2]], Route)
-  break;
-  }
+fetch('http://cursussen.uantwerpen.be/Home/Level')
+    .then(response => response.json())
+    .then((jso) => json = jso)
+    .then(() => CreatePage(''));
+
+
+function CreatePage(Route) {
+
+    let path = Route.split("/").map(Number);
+    console['info']('path :' + path);
+
+    switch (path.length) {
+        case 1:
+
+            FillWithData(json, Route)
+            break;
+        case 2:
+            FillWithData(json.Items[path[1]], Route)
+            break;
+        case 3:
+            FillWithData(json.Items[path[1]].Items[path[2]], Route)
+            break;
+        case 4:
+            FillWithData(json.Items[path[1]].Items[path[2]].Items[path[3]], Route)
+            break;
+    }
 }
-function FillWithData(json, Route)
-{
-   let page = new Page({ title: json.Title}).appendTo(navigationView);
-  
-  for (var i = 0; i < json.Items.length; i++)
-  {
-    new Button({
-      left: 16, right: 16, top: 'prev()',
-      text: json.Items[i].Item,
-      id: Route + "/" + i
-    })
-      .on('select', ({target}) => { CreatePage(target.id) })
-      .appendTo(page);
-  } 
+
+function FillWithData(json, Route) {
+    let page = new Page({
+        title: json.Title
+    }).appendTo(navigationView);
+
+    for (var i = 0; i < json.Items.length; i++) {
+        new Button({
+                left: 16,
+                right: 16,
+                top: 'prev()',
+                text: json.Items[i].Item,
+                id: Route + "/" + i
+            })
+            .on('select', ({
+                target
+            }) => {
+                CreatePage(target.id)
+            })
+            .appendTo(page);
+    }
 }
